66ab0d1063abe7e68b6d5281859ffecf9d7564a7
Mauricio Alvarez <mauricioalvarez.or@gmail.com>
2023-03-26 16:56:58 -0300

    agregado textbox

--- ProyectoAlfa/Form1.Designer.cs
+++ ProyectoAlfa/Form1.Designer.cs
@@ -29,23 +29,14 @@ namespace ProyectoAlfa
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(120, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(359, 149);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Boton Uno";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -68,7 +59,7 @@ namespace ProyectoAlfa
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(13, 85);
+            this.button3.Location = new System.Drawing.Point(270, 149);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -104,18 +95,25 @@ namespace ProyectoAlfa
             this.checkBox3.Text = "checkBox3";
             this.checkBox3.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 339);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -124,14 +122,13 @@ namespace ProyectoAlfa
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
